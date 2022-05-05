0403b80238c73ca523b829310fbfd752a0f58cc6
Sagefully <max.lundell+github@outlook.com>
2022-05-05 12:41:01 +0200

    build: :rocket: 123

--- src/lib/cards.svelte
+++ src/lib/cards.svelte
@@ -1,11 +1,67 @@
-<div class="flex p-16">
-  <div class="grid grid-cols-3 w-full bg-neutral shadow-lg"> 
+<div class="md:container md:mx-auto ">
+    
+    <div class="justify-items-center grid grid-cols-3 grid-rows-2 bg-primary gap-y-6 p-6">
 
-    <div class="card w-96 bg-base">
-    <img src="https://api.lorem.space/image/shoes" alt="Shoes" />
+        <div class="card w-96 bg-base-100 shadow-xl image-full">
+            <figure><img src="" alt="Shoes" /></figure>
+            <div class="card-body">
+              <h2 class="card-title">Köp</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+              <h2 class="card-title">Boka andelar</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+              <h2 class="card-title">Praktik</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="card w-96 bg-base-content  text-primary-content">
+            <div class="card-body">
+              <h2 class="card-title">Filosofi</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <button class="btn">Buy Now</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+              <h2 class="card-title">Samarbeten</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+              <h2 class="card-title">Certifieringar</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+
     </div>
-
-
-
-  </div>
 </div>
--- src/lib/hero.svelte
+++ src/lib/hero.svelte
@@ -1,10 +1,9 @@
-<div class="hero min-h-16 bg-secondary-content" style="background-image: url('https://thumbs.dreamstime.com/b/agricultural-landscape-vegetable-plantations-growing-organic-vegetables-field-farm-agriculture-potatoes-carrot-146822667.jpg');">
-  <div class="hero-overlay bg-opacity-80"></div>
-  <div class="text-neutral-content ">
-    <div class="md:max-w-md lg:max-w-max p-32">
-      <h1 class="mb-5 text-2xl font-bold ">Färska, närodlade & ekologiska grönsaker</h1>
-      <p class="mb-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis dignissimos fugiat non? Beatae necessitatibus dolor nemo unde illum adipisci quo quis accusantium animi laboriosam suscipit aspernatur, iure, quos ducimus nisi.</p>
-      <!-- <button class="btn btn-primary">Get Started</button> -->
+<div class="hero min-h-fit rounded-t-md " style="background: url(https://yardyum.com/storage/app/media/Correct%20way%20to%20aerate%20garden.jpg) bottom;">
+    <div class="hero-overlay bg-opacity-70 rounded-t-md"></div>
+    <div class="hero-content text text-neutral-content">
+      <div class="max-w-screen-lg p-16">
+        <h1 class="mb-5 text-5xl font-bold">Närodlat & ekologiskt.</h1>
+        <p class="mb-5">Få direkt eller förplanerad tillgång till grönetiska </p>
+      </div>
     </div>
   </div>
-</div>
